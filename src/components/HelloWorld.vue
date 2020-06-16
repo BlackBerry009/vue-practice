076e8dc8103816b3ec045e0de50f2c964f4a5374
lyg <765042894@qq.com>
2020-06-16 15:29:32 +0800

    finish

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,187 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="goods" v-for="(goods,row) in goodsList" :key="goods.id">
+      <div class="title">{{ goods.title}}</div>
+      <ul class="type-list">
+        <li
+          class="type"
+          v-for="(list,i) in goods.typeList"
+          :key="i"
+          :class="{
+            active: goods.index === i 
+        }"
+          @click="handleClick(i,goods,list,row)"
+        >{{ list }}</li>
+      </ul>
+    </div>
+    <div class="choose-type">
+      <div>已选条件：</div>
+      <span class="no-goods" v-if="!showFlag">暂时没有选择过滤的条件</span>
+      <ul class="filter-list" v-else>
+        <li v-for="(goods,index) in filterList" :key="index">{{ goods }}
+          <span class="delete-goods" @click="deleteGoods(index)">x</span>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: "HelloWorld",
+  data() {
+    return {
+      goodsList: [
+        {
+          title: "上装",
+          typeList: [
+            "全部",
+            "针织衫",
+            "毛呢外套",
+            "T恤",
+            "羽绒服",
+            "棉衣",
+            "卫衣",
+            "风衣"
+          ],
+          id: 1
+        },
+        {
+          title: "裤装",
+          typeList: [
+            "全部",
+            "牛仔裤",
+            "小脚/铅笔裤",
+            "休闲裤",
+            "打底裤",
+            "哈伦裤"
+          ],
+          id: 2
+        },
+        {
+          title: "裙装",
+          typeList: ["全部", "连衣裙", "半身裙", "长袖连衣裙", "中长款连衣裙"],
+          id: 3
+        }
+      ],
+      filterList: {},
+      showFlag: false
+    };
+  },
+  created() {
+    this.goodsList.forEach(item => this.$set(item, "index", 0));
+  },
+  methods: {
+    handleClick(i,goods,list,row){
+      this.showFlag = true;
+      goods.index = i;
+      if(list == '全部'){
+        this.$delete(this.filterList,row);
+        this.checkShowFilter();
+        return;
+      };
+      this.$set(this.filterList,row,list);
+    },
+    deleteGoods(key){
+      this.$delete(this.filterList,key)
+      this.goodsList[key].index = 0;
+      this.checkShowFilter();
+    },
+    checkShowFilter(){
+      const a = JSON.stringify(this.filterList)
+      this.showFlag = a !== '{}'
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+* {
+  margin: 0;
   padding: 0;
 }
+
+ul,
 li {
-  display: inline-block;
-  margin: 0 10px;
+  list-style: none;
 }
-a {
-  color: #42b983;
+
+#app {
+  width: 550px;
+  margin: 80px auto;
+  padding: 5px 15px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  font-size: 12px;
+  color: #666;
+}
+
+#app .goods {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px dashed #eee;
+  text-indent: 2em;
+}
+
+#app .goods:last-child {
+  border-bottom: none;
+}
+
+#app .goods .type-list {
+  display: flex;
+  text-indent: 0;
+}
+
+#app .goods .type-list .type {
+  margin: 15px 7px;
+  padding: 5px 6px;
+  border-radius: 3px;
+  color: #039;
+  cursor: pointer;
+}
+
+#app .goods .type-list .type:hover {
+  color: #f60;
+  background-color: #f3edc2;
+}
+
+#app .goods .type-list .type.active {
+  color: #fff;
+  background-color: #f60;
+}
+
+#app .choose-type {
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+}
+
+#app .choose-type .no-goods {
+  color: #999;
+  font-weight: normal;
+  padding: 20px;
+}
+
+#app .choose-type .filter-list {
+  display: flex;
+  font-weight: normal;
+}
+
+#app .choose-type .filter-list li {
+  margin: 15px 10px;
+  padding: 5px 8px;
+  border-radius: 3px;
+  color: #fff;
+  background-color: #f60;
+  cursor: pointer;
+}
+
+#app .choose-type .filter-list li .delete-goods {
+  opacity: 0.5;
+}
+
+#app .choose-type .filter-list li .delete-goods:hover {
+  opacity: 1;
 }
 </style>
